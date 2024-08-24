3ef5d7ae18ad95747f81e9d3db472747f9916df1
Jan-Fcloud <jan@jansbyte.com>
2024-08-24 20:42:18 +0200

    initial layout commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+    <a href="{base}/blog">Blog</a>
+</nav>
+
+<main>
+    <slot />
+</main>
