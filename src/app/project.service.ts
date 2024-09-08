31a84ceac62a344b7e3b0d48f449d579121c0001
Jan-Fcloud <jan@jansbyte.com>
2024-09-08 15:03:05 +0200

    remove comment

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -8,13 +8,6 @@ import { forkJoin, map, Observable } from 'rxjs';
 export class ProjectService {
   constructor(private http: HttpClient) { }
 
-  /* getRepos(namesArray: string[]){
-    let jsons: Observable<any>[] = [];
-    namesArray.forEach(element => {
-      jsons.push(this.http.get<any>(`https://api.github.com/repos/${element}`));
-    });
-    return jsons;
-  } */
     getRepoData(repoNames: string[]): Observable<any[]> {
       const requests = repoNames.map(name => 
         this.http.get(`https://api.github.com/repos/${name}`).pipe(
