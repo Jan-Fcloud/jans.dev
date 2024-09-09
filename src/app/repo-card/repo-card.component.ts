dadbbe8a011621ed311a4ae1e0f8c1dbe8f270aa
Jan-Fcloud <jan@jansbyte.com>
2024-09-09 17:02:43 +0200

    solid icons, lang on projects with color

--- src/app/repo-card/repo-card.component.ts
+++ src/app/repo-card/repo-card.component.ts
@@ -1,11 +1,35 @@
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUpRightFromSquare, faStar, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { NgStyle } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-repo-card',
   templateUrl: './repo-card.component.html',
-  styleUrls: ['./repo-card.component.scss']
+  styleUrls: ['./repo-card.component.scss'],
+  imports: [FontAwesomeModule, NgStyle]
 })
 export class RepoCardComponent {
+
+  langColors : {[key: string]: string} = {
+    'C#': "#178600",
+    'C++': "#f34b7d",
+    'Python' : "#3572A5",
+    'TypeScript' : "#3178c6",
+    'JavaScript' : "#f1e05a",
+    'Rust' : "#dea584",
+    'PHP' : "#4F5D95",
+    'HTML' : "#e34c26",
+    'CSS' : "#563d7c",
+  }
+
+  iStar = faStar;
+  iExternal = faArrowUpRightFromSquare;
+  iCircle = faCircle;
+
+  dynamicColor() : string{
+    return this.langColors[this.repoData.language] || "#000";
+  }
   @Input() repoData: any;
 }
