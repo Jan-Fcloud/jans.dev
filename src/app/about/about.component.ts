1157cf6ed47f1ae5cedf935fdba7796d05e702c7
Jan <jan@jans.dev>
2024-09-18 20:05:32 +0200

    move projects to about and add angular ghpages

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgFor, CommonModule } from '@angular/common';
+
+import { RepoCardComponent } from '../repo-card/repo-card.component';
+import { Repo, repoData } from 'src/public/repos';
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   standalone: true,
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  imports: [RepoCardComponent, NgFor, CommonModule],
 })
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  repos$: Repo[] = new repoData().data;
+  ngOnInit() {
+    this.repos$ = new repoData().data
+  }
 
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
   { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
-  { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent) },
-  { path: 'projects', loadComponent: () => import('./projects/projects.component').then(m => m.ProjectsComponent) }
+  { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent) }
 ];
 
 @NgModule({
