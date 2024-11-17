41907dc48c652cc19a589dad7895a4a17dde2a5a
Jan-Fcloud <jan@jansbyte.com>
2024-11-17 16:01:50 +0100

    dev build fix, new post prep

--- src/public/posts.ts
+++ src/public/posts.ts
@@ -7,6 +7,7 @@ export interface Post {
 
 export class postData{
 
+    static assetPath = "/assets";
     static basePath = "/public/posts";
 
     static data : Post[] = [
@@ -22,8 +23,14 @@ export class postData{
         {
             title: "Hello World",
             path: `${this.basePath}/hello-world.md`,
-            img_url: "https://avatars.githubusercontent.com/u/47220014?v=4&size=64",
+            img_url: "https://avatars.githubusercontent.com/u/47220014?v=4&size=256",
             description: "Welcome to the blog! This is the first (test) post.",
+        },
+        {
+            title: "How I uploaded this Anuglar app to GitHub Pages",
+            path: `${this.basePath}/angular-to-ghpages.md`,
+            img_url: `${this.assetPath}/angular-to-ghpages.png`,
+            description: "A comprehensive guide on how to deploy an Angular app to GitHub Pages.",
         },
 
 
---
+++ src/public/posts/angular-to-ghpages.md
@@ -0,0 +1,1 @@
+# How I uploaded this Anuglar app to GitHub Pages
